cbb13b81b107ffcabb292a6af5059023aad6064b
Remiat Alexandre <134190489+aremiat@users.noreply.github.com>
2024-10-24 15:26:30 +0200

    Add files via upload

--- StrategyBank.py
+++ StrategyBank.py
@@ -58,10 +58,9 @@ class RandomStrategy(Strategy):
         return pd.Series(weights, index=historical_data.columns)
 
 class ValueStrategy:
-    def __init__(self, historical_data, lookback_period=90, alpha = 1):
+    def __init__(self, historical_data, lookback_period=365, alpha = 1):
         self.lookback_period = lookback_period
         self.historical_data = pd.read_csv(historical_data)
-        self.lookback_period = lookback_period
         self.historical_data['Date']  = pd.to_datetime(self.historical_data['Date'])
         self.historical_data.set_index('Date', inplace=True)
         self.alpha = alpha
@@ -69,7 +68,8 @@ class ValueStrategy:
     def rank_assets(self):
         """
         on classe les actifs puis on leur ajoute un parametre alpha
-        qui determine le coefficient de risque du portefeuill
+        qui determine le coefficient de risque du portefeuille
+        On short les actifs qui ont le plus performé car phénoméne de mean reversion
         """
         last_prices = self.historical_data.iloc[-1]  # Dernier prix de chaque actif
         prices_one_year_ago = self.historical_data.iloc[-self.lookback_period]  # Prix d'il y a un an
@@ -84,42 +84,23 @@ class ValueStrategy:
 
         # latest_returns = total_returns.iloc[-1]
         ranked_assets = coef_asset.rank(ascending=False, method='first').sort_values()
+        
         num_assets = ranked_assets.count()
         sum_of_ranks = ranked_assets.sum()
         average = sum_of_ranks / num_assets
         weights = (ranked_assets - average) * self.alpha
         
-        # Normalisation pour que les poids soient entre -1 et 1
-        max_weight = weights.max()
-        min_weight = weights.min()
-        
-        # Normaliser les poids
-        weights = (weights - average) / (max_weight - min_weight)  # Ajustement centré
+        total_abs_ranks = sum(abs(weights))
 
-
-        # Ajustement final pour garantir que la somme des poids soit 0
-        weights = weights - weights.mean()  # Centre autour de 0
-
-        # S'assurer que les poids sont dans l'intervalle [-1, 1]
-        weights = weights.clip(lower=-1, upper=1)
-        
-        # Ajustement final pour garantir que la somme des poids soit 1
-        if weights.sum() != 0:
-            weights /= weights.sum()  # Normaliser pour que la somme soit 1
-
-        # Remise à l'échelle pour que la somme soit 1
-        weights = (weights + 1) / 2  # Passer de [-1, 1] à [0, 1]
-        weights /= weights.sum()  # Normaliser pour que la somme soit 1
-        
-        
-        return weights
+        normalized_weights = weights / total_abs_ranks
+               
+        return normalized_weights
 
 class MomentumStrategy:
     
     def __init__(self, historical_data, lookback_period=365):
         self.lookback_period = lookback_period
         self.historical_data = pd.read_csv(historical_data)
-        self.lookback_period = lookback_period
         self.historical_data['Date']  = pd.to_datetime(self.historical_data['Date'])
         self.historical_data.set_index('Date', inplace=True)
     
@@ -152,6 +133,44 @@ class MomentumStrategy:
         
         return weights
 
-class MinVolStrategy(RankedStrategy):
-    pass
+class MinVolStrategy:
+    
+    def __init__(self, historical_data):
+        self.historical_data = pd.read_csv(historical_data)
+        self.historical_data['Date']  = pd.to_datetime(self.historical_data['Date'])
+        self.historical_data.set_index('Date', inplace=True)
+        
+    def rank_assets(self):
+        
+        returns = self.historical_data.pct_change().dropna()
+        # Calculer la variation quotidienne de la performance pour les actifs sélectionnés
+        daily_variation = returns.diff()
+        print(daily_variation)
+        
+        # Calculer les poids relatifs en divisant la variation quotidienne de chaque actif par la variation totale
+        relative_weights = daily_variation.div(daily_variation.sum(axis=1), axis=0)
+        relative_weights = relative_weights.iloc[-1]
+        # Remplacer les NaN par des zéros
+        self.adjusted_weights_over_time = relative_weights.fillna(0)
+        
+        return relative_weights
+    
+    
 
+data = pd.read_csv(r"C:\Users\admin\Desktop\cours dauphine\poo\projet\data\database.csv")
+data['Date']  = pd.to_datetime(data['Date'])
+data.set_index('Date', inplace=True)
+
+data = data.pct_change().dropna()
+total_returns = data.rolling(window=365).apply(lambda x: (1 + x).prod() - 1)
+ 
+a = MomentumStrategy(r"C:\Users\admin\Desktop\cours dauphine\poo\projet\data\database.csv")
+a.rank_assets()
+
+a = ValueStrategy(r"C:\Users\admin\Desktop\cours dauphine\poo\projet\data\database.csv")
+
+a.rank_assets()
+
+a = MinVolStrategy(r"C:\Users\admin\Desktop\cours dauphine\poo\projet\data\database.csv")
+
+a.rank_assets()
